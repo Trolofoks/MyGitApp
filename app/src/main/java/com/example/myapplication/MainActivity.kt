a9d2ad6a94d0296ce88db91975c8ccd06e8b2473
Trolofoks <Trolofoks@gmail.com>
2022-11-09 14:13:26 +0700

    day3

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,31 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.placeHolder, MainFragment.newInstance())
-            .commit()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+
+    }
+    private fun init(){
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId){
+                R.id.delete -> {}
+                R.id.add -> {
+                    startActivity(Intent(this,AddActivity::class.java))
+                }
+            }
+            true
+        }
+
     }
 }
